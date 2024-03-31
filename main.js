a89e6e8b28a6f5e8623f08da38217b7d98425453
larryp89 <42215520+larryp89@users.noreply.github.com>
2024-03-31 20:38:54 +0100

    Colour change and title.

--- main.js
+++ main.js
@@ -7,7 +7,11 @@ function showGrid(userChoice) {
     let individualSquare = setGridArea(gridDimension);
     let newDiv = document.createElement("div");
     newDiv.setAttribute("class", "grid-div");
-    newDiv.setAttribute("style", `border: 0.5px black solid; width: ${individualSquare}; height: ${individualSquare}; box-sizing: border-box;`);    containerDiv.appendChild(newDiv);
+    newDiv.setAttribute(
+      "style",
+      `border: 0.5px black solid; width: ${individualSquare}; height: ${individualSquare}; box-sizing: border-box;`
+    );
+    containerDiv.appendChild(newDiv);
     // add event listener
     newDiv.addEventListener("mouseover", colorchange);
   }
@@ -15,7 +19,10 @@ function showGrid(userChoice) {
 
 // colour change function
 function colorchange() {
-  this.style.background = "grey";
+  const r = Math.round(Math.random() * 255);
+  const g = Math.round(Math.random() * 255);
+  const b = Math.round(Math.random() * 255);
+  this.style.background = `rgb(${r}, ${g}, ${b})`;
 }
 
 function setGridArea(widthHeight) {
